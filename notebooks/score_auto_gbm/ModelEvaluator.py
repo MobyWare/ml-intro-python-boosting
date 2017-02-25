8205c3762ddd78df6bed5b1656c02ab572caf9e4
Mohinder Dick (UPMC Mobile) <dickm@upmc.edu>
2017-02-24 22:30:17 -0500

    Tested saved model. Eval model class. scaling.

--- notebooks/score_auto_gbm/ModelEvaluator.py
+++ notebooks/score_auto_gbm/ModelEvaluator.py
@@ -21,40 +21,50 @@ class ModelEvaluator(object):
             return False
         
 
-    def get_predict_vs_actuals(self, model, features, actuals, preprocessor=None):
+    def get_predict_vs_actuals(self, model, features, actuals=None, preprocessor=None):
         result = []
+        prediction_list = []
         
-        if preprocessor is None:
-            actual_features = features
-        else:
-            actual_features = preprocessor.fit(features)
+        if actuals is None:
+            for row in features.to_dict(orient='records'):
+                try:
+                    prediction = list(model.predict(pd.DataFrame([row])))[0]
+                except ValueError:
+                    continue
+                
+                if self.is_number(prediction) == True:
+                    prediction_list.extend([float(prediction)])
 
-        for idx, row in enumerate(actual_features.to_dict(orient='records')):
-            try:
-                prediction = list(model.predict(pd.DataFrame([row])))[0]
-            except ValueError:
-                continue
-            
-            if is_number(data.ix[idx,0]) == True and is_number(prediction) == True:
-                result.extend([(float(prediction), float(actuals[idx]))])
+            result = zip(np.array(prediction_list), np.array([sum(prediction_list)/len(prediction_list)] * len(prediction_list)))        
+        else:            
+            for idx, row in enumerate(features.to_dict(orient='records')):
+                try:
+                    prediction = list(model.predict(pd.DataFrame([row])))[0]
+                except ValueError:
+                    continue
+                
+                if self.is_number(actuals[idx]) == True and self.is_number(prediction) == True:
+                    result.extend([(float(prediction), float(actuals[idx]))])
 
-        if(len(result) < len(actuals)):
-            print(
-                'Some predicions may have failed. # of successful predictions: %d, # of actual values: %d' % 
-                (len(actuals), len(result)))       
+            if(len(result) < len(actuals)):
+                print(
+                    'Some predicions may have failed. # of successful predictions: %d, # of actual values: %d' % 
+                    (len(actuals), len(result)))    
+
+        return result   
     
     def get_rmse_summary(self, predction_tuple_array):
         total = len(predction_tuple_array)
         squared_error = map(lambda errorTuple: (errorTuple[0] - errorTuple[1])**2, predction_tuple_array)
         rmse = (sum(squared_error)/total)**0.5
 
-        average_prediction = um(map(lambda errorTuple: errorTuple[1], predction_tuple_array))/total
+        average_prediction = sum(map(lambda errorTuple: errorTuple[1], predction_tuple_array))/total
         standard_squared_error = map(lambda errorTuple: (average_prediction - errorTuple[1])**2, predction_tuple_array)
         standard_rmse = (sum(standard_squared_error)/total)**0.5
 
         return rmse, standard_rmse, total
 
-    def get_feature_importance_tuples(self, features, model, sort=True):
+    def get_feature_importance_tuples(self, model, features, sort=True):
         if(model.feature_importances_ is None):
             raise ValueError('The model must have important features method.')
         else:
